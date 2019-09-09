819fcd6f8d0ada2e0500e2e9503193779636fb24
Khristina1 <43894741+Khristina1@users.noreply.github.com>
2019-09-09 16:32:40 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,24 +1,20 @@
-const display = document.querySelector('.calculator .display'); // посилання на дисплей
- document.querySelectorAll(' .numbers button')
-  .forEach(numbers => numbers.addEventListener('click' , numbersPressed)); // кнопка натиснута
-
-   function numbersPressed(ev){
-display.value += ev.target.innerText; // кнопка на яку ми нажали показує на дисплей(target )
-   }
-
- document.querySelectorAll('.operation button')
-.forEach(operation => operation.addEventListener('click', operationPressed));
-function operationPressed(ev){
-       display.value += ev.target.innerText;
+const display = document.querySelector('.calculator .display');
+document.querySelectorAll(' .numbers button').forEach(numbers=>numbers.addEventListener('click', numbersPressed));
+function numbersPressed(ev) {
+    display.value += ev.target.innerText;
+   
 }
- document.querySelectorAll('.equal')
- .forEach(equal => equal.addEventListener('click', equalPressed));
-  function equalPressed(){
-        display.value = eval(display.value);
-  }
 
-document.querySelectorAll('.reset')
-.forEach(reset => reset.addEventListener('click', resetPressed));
-  function resetPressed(){
-      display.value = ' ';
- }
+document.querySelectorAll('.operation button').forEach(operation=>operation.addEventListener('click', operationPressed));
+function operationPressed(ev) {
+    display.value += ev.target.innerText;
+}
+document.querySelectorAll('.equal').forEach(equal=>equal.addEventListener('click', equalPressed));
+function equalPressed() {
+    display.value = eval(display.value);
+}
+
+document.querySelectorAll('.reset').forEach(reset=>reset.addEventListener('click', resetPressed));
+function resetPressed() {
+    display.value = ' ';
+}
